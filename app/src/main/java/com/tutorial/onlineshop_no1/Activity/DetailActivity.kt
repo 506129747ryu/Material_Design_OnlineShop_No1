fb5289d2d7d3e3a610f2560675540b9a89b43e4e
IMP_Lab <j218305@ns.kogakuin.ac.jp>
2023-08-29 17:50:24 +0900

    Detail Activity UI add

---
+++ app/src/main/java/com/tutorial/onlineshop_no1/Activity/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.tutorial.onlineshop_no1.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tutorial.onlineshop_no1.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
